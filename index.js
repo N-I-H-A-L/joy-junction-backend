7d7d21f45b0c2f82825481a9ec60efcf9cf7a2dc
Nihal Baranwal <nihalkumar9749@gmail.com>
2024-06-08 09:26:19 +0530

    CORS update

--- index.js
+++ index.js
@@ -36,11 +36,14 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    console.log("Response Headers Set:", res.getHeaders());
-    next();
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === "OPTIONS") {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 app.use(express.json());
